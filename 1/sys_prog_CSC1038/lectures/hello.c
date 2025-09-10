5b816d8b4888629597e47aa83d4564569ec9ede0
Adam btw <adam.tait617@gmail.com>
2025-09-10 10:46:49 +0100

    systems programming and linear algebra notes

---
+++ 1/sys_prog_CSC1038/lectures/hello.c
@@ -0,0 +1,14 @@
+/* Include standard input and output */
+#include <stdio.h>
+
+/*
+Function : main
+Params   : None
+Returns  : int
+Desc     : prints the string "hellow world"
+*/
+int main() {
+  /* Begin function */
+  printf("hello, world\n");
+  return 0;
+} /* End function and program */
---
+++ 1/sys_prog_CSC1038/lectures/marking.md
@@ -0,0 +1,7 @@
+* week 8 assessment 20%
+* week 12 assessment 20%
+* final exam 40%
+* labs 20%
+  * marks for coments / presentation
+  * marks for using specified solutions eg. use linked list / pointers
+  
