43b95cacda4f0827eacfc2f3720012f1306aa12f
TaitA2 <adam.tait617@gmail.com>
2025-09-25 16:18:15 +0100

    finish reverse

--- 1/sys_prog_CSC1038/labs/labsheet3/lab3-string-reverse.c
+++ 1/sys_prog_CSC1038/labs/labsheet3/lab3-string-reverse.c
@@ -6,29 +6,30 @@ This program reverses the string passed as the first argument
 #include <stdio.h>
 #include <string.h>
 
-char *reverse(char *s);
+void reverse(char *s);
+
 int main(int argc, char *argv[]){
   if (argc != 2){
     printf("Enter exactly one argument\n");
     return 0;
   }
-  printf("%s\n", reverse(argv[1]));
+  reverse(argv[1]);
   return 0;
 }
-char *reverse(char *s){
+
+void reverse(char *s){
   char reversed[50];
+  reversed[49] = '\0';
   int i = 0;
   while (s[i] != '\0'){
     reversed[50-i-2] = s[i];
-    // printf("%c\n",s[i]);
     i++;
   }
 
-  i++
-  while (i < 50){
+  i = 50 - i - 1;
+  while (reversed[i] != '\0'){
     printf("%c",reversed[i]);
     i++;
   }
-  printf("%s", reversed);
-  return reversed;
+  printf("\n");
 }
