f3121fb9a4c8f0f846d6684dbd31626671d235cd
TaitA2 <adam.tait617@gmail.com>
2025-09-25 15:25:45 +0100

    finish lab3 calculator

--- 1/sys_prog_CSC1038/labs/labsheet3/lab3-calculator.c
+++ 1/sys_prog_CSC1038/labs/labsheet3/lab3-calculator.c
@@ -7,19 +7,37 @@ This program is to multiply or divide two numbers as specified by the user
 #include <stdlib.h>
 #include <string.h>
 
-float mult(float x, float y);
-float div(float x, float y);
+float multiply(float x, float y);
+float divide(float x, float y);
 int main(int argc, char *argv[]){
-  method = argv[1];
-  if method == ""
+  if (argc != 4){
+    printf("Must have 3 arguments!\n");
+    return 1;
+  }
+  char *method = argv[1];
+  float x = atof(argv[2]);
+  float y = atof(argv[3]);
+  if (strcmp(method,"multiply") == 0) {
+    printf("%f\n", multiply(x,y));
+    
+  }else if (strcmp(method,"divide") == 0) {
+    if (y == 0){
+      printf("invalid\n");
+      return 0;
+    }
+    printf("%f\n", divide(x,y));
+    
+  }else {
+    printf("Invalid method: '%s'\n", method);
+  }
 
   return 0;
 }
 
-float mult(float x, float y){
+float multiply(float x, float y){
   return x * y;
 }
 
-float div(float x, float y){
+float divide(float x, float y){
   return x / y;
 }
