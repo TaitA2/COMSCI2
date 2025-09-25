1defbaeb76df76f604e2a4d192648d11d8c334f0
TaitA2 <adam.tait617@gmail.com>
2025-09-25 15:07:06 +0100

    start c lab 3

---
+++ 1/sys_prog_CSC1038/labs/labsheet3/lab3-calculator.c
@@ -0,0 +1,25 @@
+/*
+Author: Adam Tait
+Date: 25/09/25
+This program is to multiply or divide two numbers as specified by the user
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+float mult(float x, float y);
+float div(float x, float y);
+int main(int argc, char *argv[]){
+  method = argv[1];
+  if method == ""
+
+  return 0;
+}
+
+float mult(float x, float y){
+  return x * y;
+}
+
+float div(float x, float y){
+  return x / y;
+}
