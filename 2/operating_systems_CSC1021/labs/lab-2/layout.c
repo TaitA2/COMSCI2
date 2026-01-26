24c1cebe9cae88f3a7d48ff00f303bd869814335
Adam Nemesis <adam.tait617@gmail.com>
2026-01-26 15:38:39 +0000

    finish os lab 2

---
+++ 2/operating_systems_CSC1021/labs/lab-2/layout.c
@@ -0,0 +1,21 @@
+/* * @author Silberschatz, Galvin, and Gagne
+ * Operating System Concepts  - Tenth Edition
+ * Copyright John Wiley & Sons - 2018
+ */
+
+#include <stdlib.h>
+
+int x;
+int y = 15;
+
+int main(int argc, char *argv[]) {
+  int *values;
+  int i;
+
+  values = (int *)malloc(sizeof(int) * 5);
+
+  for (i = 0; i < 5; i++)
+    values[i] = i * y;
+
+  return 0;
+}
---
+++ 2/operating_systems_CSC1021/labs/lab-2/newproc-posix4.c
@@ -0,0 +1,39 @@
+/**
+ * This program forks a separate process
+ * using the fork()/exec() system calls.
+ *
+ * Figure 3.08
+ *
+ * @author Silberschatz, Galvin, and Gagne
+ * Operating System Concepts  - Tenth Edition
+ * Copyright John Wiley & Sons - 2018
+ */
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main() {
+  pid_t pid;
+
+  /* fork a child process */
+  pid = fork();
+
+  if (pid < 0) { /* error occurred */
+    fprintf(stderr, "Fork Failed\n");
+    return 1;
+  } else if (pid == 0) { /* child process */
+    printf("I am the child %d\n", pid);
+
+    while (1)
+      ;
+
+    printf("Do I execute?\n");
+  } else { /* parent process */
+    /* parent will wait for the child to complete */
+    printf("I am the parent %d\n", pid);
+  }
+
+  return 0;
+}
