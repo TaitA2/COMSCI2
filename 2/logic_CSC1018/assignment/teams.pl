34ce4ff08364a5e891a9477dbbe4ad6d54fbf771
Adam Nemesis <adam.tait617@gmail.com>
2026-04-11 13:55:40 +0100

    finish prolog assignment

--- 2/logic_CSC1018/assignment/teams.pl
+++ 2/logic_CSC1018/assignment/teams.pl
@@ -1,4 +1,5 @@
 % FACTS
+
 % team group assignments
 % group(Group, Team).
 
@@ -38,6 +39,12 @@ group(f, fc).
 group(f, fd).
 group(f, fe).
 
+
+% PREDICATES
+
+% Fixture of HomeTeam vs AwayTeam. Both teams are in the same group and are different teams. 
+fixture(HomeT, AwayT) :-  group(G, HomeT), group(G, AwayT), HomeT \== AwayT.
+
 % count how many matches are on a given day
 countDay(_, [], 0).
 countDay(D, [(_,_,D)|L], N) :- !, countDay(D,L,N1), N is N1 + 1.
@@ -53,31 +60,48 @@ countAway(_, [], 0).
 countAway(T, [(_,T,_)|L], N) :- !, countAway(T,L,N1), N is N1 + 1.
 countAway(T, [_|L], N) :- countAway(T, L, N).
 
+% no team in the schedule has too many more games as the home or away team
+% used as an ongoing check that the schedule is balanceable
+teamBalance(HT, AT, S) :-
+		countHome(HT, S, HH), countAway(HT, S, HA),     % count the home and away games of the home team
+		countHome(AT, S, AH), countAway(AT, S, AA),     % count the home and away games of the away team
+		abs(HH + 1 - HA) =< 2, abs(AH - (AA + 1)) =< 2. % both teams have no more than 2 more games as either side
+
 % a given team has the same number of home and away games
+% used as a final check that the schedule is balanced
 sameHomeAway(T, S) :- countHome(T, S, N), countAway(T, S, N). 
 
-% no team in the schedule has more than a difference of 2 between home and away games
-teamBalance(HT, AT, S) :- countHome(HT, S, HH), countAway(HT, S, HA), countHome(AT, S, AH), countAway(AT, S, AA), abs(HH + 1 - HA) =< 2, abs(AH - AA + 1) =< 2.
-
-fixture(HomeT, AwayT) :-  group(G, HomeT), group(G, AwayT), HomeT \== AwayT.
-
-% find when a team last played a fixture
+% find the day when a team last played a fixture
 lastPlayed(T, [(T,_,D)|_], D) :- !. % base case for home team
 lastPlayed(T, [(_,T,D)|_], D) :- !. % base case for away team
-lastPlayed(T, [_|L], D) :- lastPlayed(T, L, D).
+lastPlayed(T, [_|L], D) :- lastPlayed(T, L, D). % recurse
 
-% check that a given team has at least 4 days rest between fixtures
-countRest(T, D, S) :- lastPlayed(T, S, Last), Day > Last + 4.
-countRest(T, _, S) :- \+ lastPlayed(T, S, _). % team T has not played yet, any value of D is ok
+% check that a given team has at least 4 days rest since their last fixture
+countRest(T, D, S) :- lastPlayed(T, S, Last), D >= Last + 5. % team T has played before so add 5 to their last day
+countRest(T, _, S) :- \+ lastPlayed(T, S, _).                % team T has not played yet so any day D is ok
 
-% assign a suitable day to each fixture in the schedule
+% pick a valid fixture from the remaining list
+getFixture([(H, A)|L], D, S, (H,A), L) :-
+		countDay(D, S, N), N < 3,               % no more than 3 matches on the same day
+		countRest(H, D, S), countRest(A, D, S), % both teams have at least 4 rest days
+		teamBalance(H, A, S).                   % both teams have a similar enough amount of home and away fixtures
+getFixture([F|L], D, S, Fix, [F|L1]) :- getFixture(L, D, S, Fix, L1). % skip the head and try the rest
+
+% assign a day to each fixture in the schedule
 assignDays([], _, S, S).
-assignDays([(H, A)|L], D, S, S1) :- countDay(Day, S, N), N < 3, countRest(H, D, S), countRest(A, D, S), teamBalance(H, A, S), !, assignDays(L, D, [(H, A, D)], S1).
-assignDays(Fixtures, D, S, S1) :- D1 is D + 1, assignDays(Fixtures, D1).
+assignDays(Fixtures, D, S, S1) :-
+		Fixtures \= [], % fixtures is not an empty list
+		getFixture(Fixtures, D, S, (H,A), L) -> % if there's a valid fixture for day D,
+		assignDays(L, D, [(H,A,D)|S], S1) ;     % place it and recurse on the same day
+		D1 is D + 1,                            % otherwise no fixture fits D so increment it
+		assignDays(Fixtures, D1, S, S1).        % retry with next value of D
 
 % schedule(S)
 % S is a list of fixtures
-% fixture is (homeTeam, awayTeam, day)
-
-schedule([(HomeT, AwayT, _)]) :- fixture(HomeT, AwayT).
-schedule([(HomeT, AwayT, Day) | L]) :-  sameHomeAway(HomeT, [(HomeT, _, _)|L]), sameHomeAway(AwayT, [(_, AwayT, _)|L]), countDay(Day, [(_, _, Day)|L], N2), !, N2 =< 3, fixture(HomeT, AwayT), schedule(L).
+% Every fixture has two different teams from the same group
+% Every team has at least 4 days between fixtures
+% Every team has the same number of home and away games
+schedule(S) :-
+		findall((HT,AT), fixture(HT,AT), Fixtures), % get all valid fixtures as Fixtures
+		assignDays(Fixtures, 1, [], S),             % give each fixture a valid day
+		forall(group(_, T), sameHomeAway(T, S)).    % check every team in the schedule has the same amount of home and away games
